76635c237b19c1c84cd23fe7566a45a00e38bd56
Sierra Wallace <sskwallace@gmail.com>
2020-12-24 01:24:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,41 +34,7 @@ class Student
     end.first
   end
 
-  def self.students_below_12th_grade
-    sql = <<-SQL
-    SELECT * 
-    FROM students 
-    WHERE students.grade < 12
-    SQL
-    DB[:conn].execute(sql).collect do |row|
-      self.new_from_db(row)
-    end
-  end
-
-  def self.first_X_students_in_grade_10
-    sql = <<-SQL
-    SELECT * 
-    FROM students
-    WHERE students.grade = 10
-    LIMIT 1
-    SQL
-    DB[:conn].execute(sql).collect do |row|
-      self.new_from_db(row)
-    end.first
-  end	
-
-
- def self.all_students_in_grade_9
-   sql = <<-SQL
-   SELECT *
-   FROM students
-   WHERE students.grade = 9
-   SQL
-   DB[:conn].execute(sql).collect do |row|
-    self.new_from_db(row)
-  end
-end
-
+  
   
   def save
     sql = <<-SQL
